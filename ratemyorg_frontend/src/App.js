adffefc48fd7a20817f359c8fdb95d923d784706
Vishal Kantharaju <vishal.kantharaju@gmail.com>
2023-03-03 17:57:40 -0600

    navbar template added

--- ratemyorg_frontend/src/App.js
+++ ratemyorg_frontend/src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-// import Navbar from './Components/Navbar';
+import Navbar from './Components/Navbar';
 import OrgList from './Components/OrgList.js';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
--- ratemyorg_frontend/src/Components/Navbar.js
+++ ratemyorg_frontend/src/Components/Navbar.js
@@ -1,10 +1,13 @@
-// import React from 'react'
-
-function usrClick() {
-    // placeholdder
-    alert('You have clicked a button :)');
-}
-
 export default function Navbar() {
-    
+    return <nav className="nav">
+        <a href="/">Rate My Professor</a>
+        <ul>
+            <li>
+                <a href="/login">Log In</a>
+            </li>
+            <li>
+                <a href="/login">Sign Up</a>
+            </li>
+        </ul>
+    </nav>
 }
