075490c1b4258b63c265109b406c2f4c1e38240a
kush78910 <kushkg@gmail.com>
2023-03-07 17:54:42 -0600

    Commiting searchBar and App.js changes

--- ratemyorg_frontend/src/App.js
+++ ratemyorg_frontend/src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home.js';
+import React, {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [page, setPage] = useState("Home");
+
+  return page === "Home" ? (
+    <div>
+      <Home/>
     </div>
-  );
+  ) : (
+    <div>
+      <p>not done yet</p>
+    </div>);
 }
 
 export default App;
