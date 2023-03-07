414079558bcf61618aebbfdb9ea535dbc3156b6a
Vishal Kantharaju <vishal.kantharaju@gmail.com>
2023-03-07 16:54:54 -0600

    hover feature added

--- ratemyorg_frontend/src/Components/Navbar.js
+++ ratemyorg_frontend/src/Components/Navbar.js
@@ -1,15 +1,23 @@
 import logo from '../placeholder_logo.png'
 export default function Navbar() {
     return <nav className="nav">
-        <a href="/">
+        <a href="/" className='rmplogo'>
           <img src={logo}></img>
         </a>
         <ul>
             <li>
-                <a href="/login">Log In</a>
+                <a href="/login" className='logintext'>
+                  <span className='logintexttext'>
+                    Log In
+                  </span>
+                </a>
             </li>
             <li>
-                <a href="/login">Sign Up</a>
+                <a href="/signup" className='signuptext'>
+                  <span className='signuptexttext'>
+                    Sign Up
+                  </span>
+                </a>
             </li>
         </ul>
     </nav>
